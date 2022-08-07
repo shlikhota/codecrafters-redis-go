75ded986118017191b77f0fa785e09e5dea50104
Evgenii Shlikhota <shlikhota@gmail.com>
2022-08-07 18:45:32 +0200

    parse RESP protocol (arrays only)

--- app/server.go
+++ app/server.go
@@ -5,8 +5,48 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type request struct {
+	message []string
+}
+
+func (r *request) parse(scanner *bufio.Scanner, size int) {
+	for i := 0; i < size; i++ {
+		if ok := scanner.Scan(); !ok {
+			return
+		}
+
+		dataInfo := scanner.Text()
+		dataType, strSize := dataInfo[0], dataInfo[1:]
+		if dataType != '$' {
+			fmt.Printf("Wrong type of data (%c), only string ($) is allowed\n", dataType)
+			return
+		}
+
+		if ok := scanner.Scan(); !ok {
+			return
+		}
+
+		data := scanner.Text()
+		size, err := strconv.Atoi(string(strSize))
+		if err != nil || len(data) != size {
+			fmt.Printf("Error parsing data (len=%d): %s\n", size, err)
+			return
+		}
+		r.message = append(r.message, data)
+	}
+	fmt.Printf("Received: %#v\n", r.message)
+}
+
+func (r *request) command() (command string) {
+	if len(r.message) < 1 {
+		return
+	}
+	return strings.ToLower(r.message[0])
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -31,26 +71,48 @@ func main() {
 func processConnection(c net.Conn) {
 	scanner := bufio.NewScanner(c)
 	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		msg := scanner.Bytes()
-		response := proccessRequest(msg)
-		c.Write(response)
+	for {
+		ok := scanner.Scan()
+		msg := scanner.Text()
+		if !ok || len(msg) < 2 {
+			if scanner.Err() == nil {
+				fmt.Printf("Connection with %s has been closed!\n", c.RemoteAddr())
+				return
+			}
+			fmt.Printf("Error while reading: %s\n", scanner.Err())
+			continue
+		}
+
+		msgType, msgSize := msg[0], msg[1:]
+		switch msgType {
+		// Parse array
+		case '*':
+			size, err := strconv.Atoi(string(msgSize))
+			if err != nil {
+				fmt.Printf("Error while reading size of array: %s\n", err)
+			}
+			r := request{}
+			r.parse(scanner, size)
+			c.Write(proccessRequest(r))
+		default:
+			c.Write(errorResponse("unknown prefix '%+v'", msgType))
+		}
 	}
-	msg := scanner.Bytes()
-	response := proccessRequest(msg)
-	c.Write(response)
-	fmt.Printf("Connection with %s has been closed!\n", c.RemoteAddr())
 }
 
-func proccessRequest(msg []byte) (response []byte) {
-	cmd := strings.ToLower(string(msg))
+func proccessRequest(req request) (response []byte) {
+	cmd := req.command()
 	switch cmd {
 	case "ping":
-		response = append(response, []byte("PONG")...)
+		response = append(response, []byte("+PONG")...)
 	default:
-		fmt.Printf("Unknown command: %s\n", cmd)
+		response = errorResponse("Unknown command: %s", cmd)
 		return
 	}
-	response = append(response, '\n')
+	response = append(response, []byte("\r\n")...)
 	return
+}
+
+func errorResponse(message string, params ...any) []byte {
+	return []byte("-ERR " + fmt.Sprintf(message, params...) + "\r\n")
 }
